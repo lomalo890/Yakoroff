78638681837554aa6e10ec4d07f6c02d2940dfeb
lomalo890 <george2066@yandex.ru>
2024-10-13 20:29:18 +0300

    gitignor_config

---
+++ settings/config.py
@@ -0,0 +1,8 @@
+import os
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), ".envs/.env"))
+    TELEGRAM_API_KEY: SecretStr = SecretStr("secret")
+    LOG_LEVEL: str = "INFO"
